c8052af58cd7e5ff291bc08516683f68ea20ccfe
Son-Bin-O <hongseokpyou@gmail.com>
2023-09-22 13:43:32 +0900

    Problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
+// function problem3(number) {
+//   let arr = Array.from({ length: number }, (_, i) => ++i);
+//   let reg = /[369]/;
+//   return arr.filter((v) => reg.test(v));
+// }
+
 function problem3(number) {
-  var answer;
+  let arr = Array.from({ length: number }, (_, i) => ++i);
+  let answer = 0;
+  for (let x of arr.join("")) {
+    if (x === "3" || x === "6" || x === "9") answer++;
+  }
   return answer;
 }
 
