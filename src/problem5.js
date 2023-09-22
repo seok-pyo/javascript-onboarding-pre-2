c0bf7c9847e559bb8370cedf978f2dcc249475ca
seok-pyo <hongseokpyou@gmail.com>
2023-09-22 14:08:40 +0900

    Problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [];
+  for (const unit of units) {
+    const count = Math.floor(money / unit);
+    money %= unit;
+    result.push(count);
+  }
+  return result;
 }
 
 module.exports = problem5;
