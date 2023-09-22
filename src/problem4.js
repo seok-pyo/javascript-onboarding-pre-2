afa8e343fc4fbf46b08d2c634c3be75ee17452ad
Son-Bin-O <hongseokpyou@gmail.com>
2023-09-22 13:44:12 +0900

    Problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  let CAP = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+  let SMA = "abcdefghijklmnopqrstuvwxyz";
+  let RE_CAP = CAP.split("").reverse().join("");
+  let RE_SMA = SMA.split("").reverse().join("");
+  for (let x of word) {
+    if (CAP.includes(x)) {
+      answer += RE_CAP[CAP.indexOf(x)];
+    } else if (SMA.includes(x)) {
+      answer += RE_SMA[SMA.indexOf(x)];
+    } else answer += x;
+  }
   return answer;
 }
 
